b81f9a9eee5a9f8bf4bf026ade2c1a76740578ad
Jackie <jackiesofir1989@gmail.com>
2021-11-22 11:03:52 +0200

    File Structure

--- api/views/file_crud.py
+++ api/views/file_crud.py
@@ -26,23 +26,23 @@ router = InferringRouter()
 @cbv(router)
 class FileCRUD:
 
-    @router.post("/item")
-    def create_item(self, item: ItemCreate) -> ItemInDB:
+    @router.post("/file")
+    def create_file(self, item: ItemCreate) -> ItemInDB:
         item_orm = ItemORM(name=item.name, owner=self.user_id)
         return ItemInDB.from_orm(item_orm)
 
-    @router.get("/item/{item_id}")
-    def read_item(self, item_id: ItemID) -> ItemInDB:
-        item_orm = get_owned_item(self.user_id, item_id)
+    @router.get("/file/{item_id}")
+    def read_file(self, item_id: ItemID) -> ItemInDB:
+        item_orm = get_owned_file(self.user_id, item_id)
         return ItemInDB.from_orm(item_orm)
 
-    @router.put("/item/{item_id}")
-    def update_item(self, item_id: ItemID, item: ItemCreate) -> ItemInDB:
-        item_orm = get_owned_item(self.user_id, item_id)
+    @router.put("/file/{item_id}")
+    def update_file(self, item_id: ItemID, item: ItemCreate) -> ItemInDB:
+        item_orm = get_owned_file(self.user_id, item_id)
         item_orm.name = item.name
         return ItemInDB.from_orm(item_orm)
 
-    @router.delete("/item/{item_id}")
-    def delete_item(self, item_id: ItemID) -> APIMessage:
-        item = get_owned_item(self.user_id, item_id)
+    @router.delete("/file/{item_id}")
+    def delete_file(self, item_id: ItemID) -> APIMessage:
+        item = get_owned_file(self.user_id, item_id)
         return APIMessage(detail=f"Deleted item {item_id}")
