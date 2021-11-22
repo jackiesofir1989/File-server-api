5e703cf58f429f38584690947eaa9e65836e08ca
Jackie <jackiesofir1989@gmail.com>
2021-11-22 12:55:31 +0200

    File Structure

--- api/views/file_crud.py
+++ api/views/file_crud.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import List
 
-from fastapi import File, UploadFile
+from fastapi import File, UploadFile, HTTPException, status
 from fastapi_utils.api_model import APIMessage
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
@@ -25,43 +25,43 @@ class FileCRUD:
     @router.post("/files/{folder_path}")
     def create_new_file(self, folder_path: DirectoryPath, file: UploadFile = File(...)) -> APIMessage:
         """Uploads a file given a path"""
-        if not folder_path.exists():
-            return APIMessage(detail=f"Folder {folder_path.name} dose not exist.")
-        if "file name not exiting":
-            return APIMessage(detail=f"File {file.filename} already exist.")
-        # TODO: file upload
-        return APIMessage(detail=f"File {file.filename} created at {folder_path.name}")
+        if self.check_path_exist(folder_path):
+            if "file name not exiting":
+                raise HTTPException(status_code=404, detail=f"File {file.filename} already exist.")
+            # TODO: file upload
+            return APIMessage(detail=f"File {file.filename} created at {folder_path.name}")
 
     @router.get("/files/{file_path}")
     def get_file(self, file_path: FilePath) -> File:
         """Retrieves a file given a path"""
-        if not file_path.exists():
-            return APIMessage(detail=f"File {file_path.name} dose not exist.")
-        # TODO: check for permissions
-        # TODO: get file
-        return file
+        if self.check_path_exist(file_path):
+            # TODO: check for permissions
+            # TODO: get file
+            return file
 
     @router.get("/folder/{folder_path}")
     def get_folder_list(self, folder_path: DirectoryPath) -> List[str]:
         """Retrieves a the content of a folder given a path"""
-        if not folder_path.exists():
-            return APIMessage(detail=f"Folder {folder_path.name} dose not exist.")
-        # TODO: folder_path.ls()
-        return list_of_names
+        if self.check_path_exist(folder_path):
+            # TODO: folder_path.ls()
+            return list_of_names
 
     @router.put("/files/{file_path}")
     def update_file(self, file_path: FilePath, file: UploadFile = File(...)) -> APIMessage:
         """Updates a existing file given a path"""
-        if not file_path.exists():
-            return APIMessage(detail=f"File {file_path.name} dose not exist.")
-        # TODO: check for permissions
-        # TODO: file upload
-        return APIMessage(detail=f"File {file.filename} created at {folder_path.name}")
+        if self.check_path_exist(file_path):
+            # TODO: check for permissions
+            # TODO: file upload
+            return APIMessage(detail=f"File {file.filename} created at {file_path.name}")
 
     @router.delete("/files/{file_path}")
     def delete_file(self, file_path: FilePath) -> APIMessage:
         """Removes a existing file given a path"""
+        if self.check_path_exist(file_path):
+            # TODO: check for permissions
+            return APIMessage(detail=f"Deleted file name {file_path.name} at {file_path}")
+
+    def check_path_exist(self, file_path: FilePath) -> bool:
         if not file_path.exists():
-            return APIMessage(detail=f"File {file_path.name} dose not exist.")
-        # TODO: check for permissions
-        return APIMessage(detail=f"Deleted file name {file_path.name} at {file_path}")
+            raise HTTPException(status_code=404, detail=f"Path {file_path.name} dose not exist.")
+        return True
